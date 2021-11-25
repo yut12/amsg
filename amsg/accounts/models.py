4ddffad979bd4014634287d476ce35db88767801
Hikage12 <grijing@gmail.com>
2021-11-25 12:11:38 +0900

    Update models.py

--- amsg/accounts/models.py
+++ amsg/accounts/models.py
@@ -3,8 +3,14 @@ from django.contrib.auth.models import AbstractUser
 from task.models import School
 
 # Create your models here.
+
+
 class CustomUser(AbstractUser):
-    school = models.ForeignKey(School,verbose_name='学校',default=0,on_delete=models.PROTECT)
+    school = models.ForeignKey(
+        School, verbose_name='学校', default=0, on_delete=models.PROTECT)
 
     class Meta:
         verbose_name_plural = 'CustomUser'
+
+
+fdxhxh
